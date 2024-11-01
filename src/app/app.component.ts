85423ac13b5495c9983ecae2b3f470201275c47a
GyabiSito <121214489+GyabiSito@users.noreply.github.com>
2024-11-01 03:13:14 -0300

    force

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TareaComponent } from "./components/tarea/tarea.component";
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TareaComponent, DragDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
